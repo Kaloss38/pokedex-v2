5dcfc9ec8c371b3de5379d19a1ca4c90a32710ac
Kaloss38 <jonathan.billard.pro@gmail.com>
2020-02-07 18:00:49 +0100

    get pokemon first data from promise, next display it

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,13 +2,21 @@ export default class Pokedex{
     
     constructor(arrPokemons){
         this.arrPokemons = arrPokemons;
-        this.count = 0;
+        this.count = 1;
+        this.promise = [];
     }
     
     async loadPokemons () {
         try {
-            const res = await fetch('https://pokeapi.co/api/v2/pokemon/1'); 
-            this.arrPokemons = await res.json();
+            while(this.count <= 802){
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.count}`); 
+                this.promise = await res.json();
+                this.arrPokemons.push({
+                    name: this.promise.name
+                });
+                this.count++;
+            }
+            
             console.log(this.arrPokemons);
         } catch (err) {
             console.error(err);
