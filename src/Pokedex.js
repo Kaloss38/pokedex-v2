09a5291486e2a58b6f76f17f657915df727d42bf
Kaloss38 <jonathan.billard.pro@gmail.com>
2020-02-07 17:28:02 +0100

    Add README file for github

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,12 +1,13 @@
 export default class Pokedex{
     
-    constructor(arrPokemons, count){
+    constructor(arrPokemons){
         this.arrPokemons = arrPokemons;
+        this.count = 0;
     }
     
     async loadPokemons () {
         try {
-            const res = await fetch('https://pokeapi.co/api/v2/pokemon/'); 
+            const res = await fetch('https://pokeapi.co/api/v2/pokemon/1'); 
             this.arrPokemons = await res.json();
             console.log(this.arrPokemons);
         } catch (err) {
